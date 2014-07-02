87a1b50f390b3fc58bd20228a363ed15d4d6186b
Emerson <viejoemer@gmail.com>
2014-07-02 08:56:48 -0500

    Difference between dict.clear() and assigning {} in Python?

---
+++ clear.py
@@ -0,0 +1,26 @@
+'''
+Created on Jul 1, 2014
+
+@author: viejoemer
+'''
+############################################################
+#How work clear in python dict?
+
+#Creating a dict with data
+
+d = {
+     "red":100,
+     "yellow":200,
+     "blue" : 300
+     }
+
+print(d)
+
+#Clean the dict 
+
+d.clear()
+
+print(d)
+
+print(d)
+
---
+++ clear_vs_newOne.py
@@ -0,0 +1,50 @@
+'''
+Created on Jul 1, 2014
+
+@author: viejoemer
+'''
+############################################################
+#Difference between dict.clear() and assigning {} in Python?
+
+#Creating a dict with data
+
+d = {
+     "red":100,
+     "yellow":200,
+     "blue" : 300
+     }
+
+#Reference to the same dict 
+
+d_clone = d
+
+print(d_clone)
+print(d)
+
+#Cleaning the dict
+
+d.clear()
+
+#This clean both of them.
+print(d_clone)
+print(d)
+
+############################################################
+d = {
+     "red":100,
+     "yellow":200,
+     "blue" : 300
+     }
+
+#if you use {} to "clean" a dict, this gonna create a new one.
+#and the other keep the values.
+
+d_clone = d
+
+d = {}
+
+print(d_clone)
+
+print(d)
+
+print(d)
