362b88432c022fd996433d9d99709f7ad6f26878
Emerson <root@emerson-N53SN>
2014-07-03 16:24:44 -0500

    How work clear in python dict?

--- clear.py
+++ clear.py
@@ -1,26 +1,23 @@
 '''
-Created on Jul 1, 2014
+Created on Jul 2, 2014
 
 @author: viejoemer
+
+How work clear in python dict?
+
+¿Cómo  funciona clear en un diccionario de python?
+
 '''
-############################################################
-#How work clear in python dict?
 
 #Creating a dict with data
-
 d = {
      "red":100,
      "yellow":200,
      "blue" : 300
      }
-
 print(d)
 
 #Clean the dict 
-
 d.clear()
-
-print(d)
-
 print(d)
 
