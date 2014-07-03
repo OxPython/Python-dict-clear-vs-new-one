2090cfbc04cf717f11244569bc6d6c1cfdadfeff
Emerson <root@emerson-N53SN>
2014-07-03 15:21:29 -0500

    Difference between dict.clear() and assigning {} in Python?

--- clear_vs_newOne.py
+++ clear_vs_newOne.py
@@ -1,13 +1,15 @@
 '''
-Created on Jul 1, 2014
+Created on Jul 2, 2014
 
 @author: viejoemer
+
+Difference between dict.clear() and assigning {} in Python?
+
+¿Diferencia entre dict.clear () y asignar {} en Python?
+
 '''
-############################################################
-#Difference between dict.clear() and assigning {} in Python?
 
 #Creating a dict with data
-
 d = {
      "red":100,
      "yellow":200,
@@ -15,21 +17,17 @@ d = {
      }
 
 #Reference to the same dict 
-
 d_clone = d
-
 print(d_clone)
 print(d)
 
 #Cleaning the dict
-
 d.clear()
 
 #This clean both of them.
 print(d_clone)
 print(d)
 
-############################################################
 d = {
      "red":100,
      "yellow":200,
@@ -38,13 +36,7 @@ d = {
 
 #if you use {} to "clean" a dict, this gonna create a new one.
 #and the other keep the values.
-
 d_clone = d
-
 d = {}
-
 print(d_clone)
-
-print(d)
-
 print(d)
